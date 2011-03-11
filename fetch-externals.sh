4362ddbd1800d0981e13ca519d9cdc76b8690d8e
ms705 <ms705@cl.cam.ac.uk>
2011-03-11 11:46:05 +0000

    Fixed Mahout URL as they no longer offer 0.3 for download; added RCCE checkout

--- fetch-externals.sh
+++ fetch-externals.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ext
 cd ext
-wget -N http://mirror.ox.ac.uk/sites/rsync.apache.org/mahout/0.3/mahout-0.3.tar.bz2
+wget -N http://www.cl.cam.ac.uk/netos/ciel/misc/mahout-0.3.tar.bz2
 if [ mahout-timestamp -ot mahout-0.3.tar.bz2 ]
 then
     tar -x -j -v --file=mahout-0.3.tar.bz2 mahout-0.3/mahout-core-0.3.jar mahout-0.3/mahout-math-0.3.jar mahout-0.3/mahout-collections-0.3.jar mahout-0.3/lib/commons-logging-1.1.1.jar mahout-0.3/lib/slf4j-api-1.5.8.jar mahout-0.3/lib/slf4j-jcl-1.5.8.jar mahout-0.3/lib/uncommons-maths-1.2.jar mahout-0.3/lib/gson-1.3.jar mahout-0.3/lib/hadoop-core-0.20.2.jar
@@ -14,3 +14,12 @@ then
     unzip google-gson-1.6-release.zip google-gson-1.6/gson-1.6.jar
     touch -r google-gson-1.6-release.zip gson-timestamp
 fi
+
+if [ -d rcce ]
+then
+	svn up rcce/
+else
+	mkdir -p rcce
+	svn co http://marcbug.scc-dc.com/svn/repository/trunk/rcce/ rcce/
+fi
+
