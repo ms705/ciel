e294e4db5adc38c4e9bb16085e305ac59fb938e6
ms705 <ms705@cl.cam.ac.uk>
2011-04-30 17:05:28 +0100

    Fixed some more conflicts

--- scripts/run_worker.sh
+++ scripts/run_worker.sh
@@ -1,25 +1,10 @@
-<<<<<<< HEAD:scripts/run_worker.sh
 #!/bin/sh
-BASE=$(dirname $(readlink -f $0))/..
-export PYTHONPATH=$PYTHONPATH:$BASE/src/python
-export PATH=$PATH:/opt/python/local/bin
-=======
-#!/bin/bash
->>>>>>> e92787982e42a6775e51f5b92fb6ff8b8a86b553:scripts/run_worker.sh
 PYTHON=${PYTHON:-python}
 BASE=$(${PYTHON} -c "import os,sys;print os.path.dirname(os.path.realpath('$0'))")/..
 export PYTHONPATH=$PYTHONPATH:$BASE/src/python
 
-<<<<<<< HEAD:scripts/run_worker.sh
-#LIGHTTPD_CONF_ARG="--lighttpd-conf $BASE/src/python/skywriting/runtime/lighttpd.conf"
-
 REL_BLOCK_LOCATION=${REL_BLOCK_LOCATION:-"store/"}
-=======
-if [[ $REL_BLOCK_LOCATION == "" ]]; then
-    REL_BLOCK_LOCATION="store/"
-fi
 ABS_BLOCK_LOCATION="$BASE/$REL_BLOCK_LOCATION"
->>>>>>> e92787982e42a6775e51f5b92fb6ff8b8a86b553:scripts/run_worker.sh
 
 MASTER=${MASTER_HOST:-http://127.0.0.1:8000}
 
@@ -45,8 +30,4 @@ export SW_C_LOADER_PATH=${BASE}/src/c/src/loader
 export CIEL_SKYPY_BASE=${BASE}/src/python/skywriting/runtime/worker/skypy
 export CIEL_SW_BASE=${BASE}/src/python/skywriting/lang
 export CIEL_SW_STDLIB=${BASE}/src/sw/stdlib
-<<<<<<< HEAD:scripts/run_worker.sh
-${PYTHON} ${BASE}/src/python/skywriting/__init__.py --role worker --master ${MASTER} --port $WORKER_PORT --staticbase $BASE/src/js/skyweb_worker/ ${LIGHTTPD_CONF_ARG} -b $BASE/$REL_BLOCK_LOCATION -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b $*
-=======
 ${PYTHON} ${BASE}/src/python/skywriting/__init__.py --role worker --master ${MASTER} --port $WORKER_PORT --staticbase $BASE/src/js/skyweb_worker/ ${HTTPD} -b $BASE/$REL_BLOCK_LOCATION -T ciel-process-aaca0f5eb4d2d98a6ce6dffa99f8254b ${EXTRA_CONF} $*
->>>>>>> e92787982e42a6775e51f5b92fb6ff8b8a86b553:scripts/run_worker.sh
